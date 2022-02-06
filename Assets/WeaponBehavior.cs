b005dfa4025816d3dda151c33368f1396292c4a7
TheLostPowers <marc-antoine.giguere@lcieducation.net>
2022-02-06 16:42:39 -0500

    Creating folder WeaponControler
    FolderCreated

--- Assets/WeaponBehavior.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class WeaponBehavior : MonoBehaviour
-{
-    public abstract void DoDamage(Vector3 position, float attackRange, float damage);
-}
