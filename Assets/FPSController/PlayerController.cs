fe5a7d36a9f5c67d9b78e56c82924ea17f953d82
Japolo04 <seb_4544@hotmail.com>
2022-02-09 14:15:13 -0500

    Controls ajusted and added more controls
    Crouch, slide, adjusted jump, run and speed or force of the different controls

--- Assets/FPSController/PlayerController.cs
+++ Assets/FPSController/PlayerController.cs
@@ -7,17 +7,23 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
 
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float baseSpeed = 40f;
     private Vector2 moveInput = Vector2.zero;
     private Vector3 playerMovement;
 
-    [SerializeField] private float runMultiplier = 2f;
-    private bool runInput = false;
+    [SerializeField] private float runMultiplier = 1.5f;
+    private bool isRunning;
+
+    [SerializeField] private float crouchMultiplier = 0.5f;
+    [SerializeField] private float slideSpeed = 12f;
 
     [SerializeField] private float jumpForce = 5f;
     private bool jumpInput = false;
+    private float airDrag = 1f;
     
     private bool isGrounded;
+    private float groundDrag = 6f;
 
     private CapsuleCollider capsule;
     private float capsuleScale;
@@ -31,11 +37,12 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Moving();
         CheckIfGrounded();
         Jumping();
+        DragValue();
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -47,15 +54,68 @@ public class PlayerController : MonoBehaviour
         jumpInput = context.performed;
     }
 
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            moveSpeed *= runMultiplier;
+            isRunning = true;
+        }
+        else if (context.canceled)
+        {
+            moveSpeed = baseSpeed;
+            isRunning = false;
+        }
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            moveSpeed *= crouchMultiplier;
+            transform.localPosition = new Vector3(transform.position.x, transform.position.y * 0.5f, transform.position.z);
+            transform.localScale = new Vector3(1f, 0.5f, 1f);
+            if (isRunning)
+            {
+                rb.AddForce(transform.forward * slideSpeed, ForceMode.VelocityChange);
+                isRunning = false;
+            }
+        }
+        else if (context.canceled)
+        {
+            moveSpeed = baseSpeed;
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
     void Moving()
     {
         playerMovement = transform.forward * moveInput.y + transform.right * moveInput.x;
-        rb.AddForce(playerMovement.normalized * moveSpeed, ForceMode.Acceleration);
+        if (isGrounded)
+        {
+            rb.AddForce(playerMovement.normalized * moveSpeed, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(playerMovement.normalized * moveSpeed * 0.1f, ForceMode.Acceleration);
+        }
     }
 
     void CheckIfGrounded()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, capsuleScale * 0.5f + 0.1f);
+    }
+
+    void DragValue()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
 
     void Jumping()
