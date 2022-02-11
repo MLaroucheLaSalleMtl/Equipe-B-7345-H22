49af1401b0f3da382a9c42ce6f2cffb5cdcd814a
TheLostPowers <marc-antoine.giguere@lcieducation.net>
2022-02-10 19:23:43 -0500

    Weapon Update Ready for Approval
    Bullet instantiation Done
    Aim down sight Done
    Shoot Animation Done
    Aim Animation Done
    Reload Animation Done
    Melee swing detection Done
    Damage system Done
    Ammo system Done (Need UI)
    Collision detection on bullets Done
    --might be forgetting some--
    Open to any questions

---
+++ Assets/WeaponDamage/DamageDone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDone : MonoBehaviour
+{
+    [SerializeField] private WeaponDamage damage;
+    [SerializeField] private LayerMask isEnviro;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            print(damage.Damage);
+        }
+        if(other.gameObject.layer == isEnviro && other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator BreakDistance()
+    {
+        yield return new WaitForSeconds(2.5f);
+        if (gameObject != null) Destroy(gameObject);
+    }
+}
