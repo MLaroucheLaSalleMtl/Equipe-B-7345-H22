d923619ae19a503dc6ace2c784f95c1104f95ca1
TheLostPowers <marc-antoine.giguere@lcieducation.net>
2022-02-14 14:10:58 -0500

    Revert "Revert "Weapon Update Ready for Approval""
    This reverts commit 494ef7fdf5e58f45c5c8b60609ff78e8e6f73143.

--- Assets/WeaponControl/ArBehavior.cs
+++ Assets/WeaponControl/ArBehavior.cs
@@ -5,45 +5,52 @@ using UnityEngine;
 public class ArBehavior : Attack
 {
     protected float resetTimeShot = 0.8f; //time between each individual shot
-    protected float range = 20.0f;
     protected float bulletSpeed = 20;
     [SerializeField] private Rigidbody bullets;
     [SerializeField] private GameObject muzzle;
+    private int currentAmmo = 30;
+
+    private void Awake()
+    {
+        base.maxBullet = 30;
+        base.noAmmo = false;
+    }
 
     public IEnumerator ShootRifle()
     {
         yield return new WaitForSeconds(0.1f);
         Rigidbody clone1 = Instantiate(bullets, muzzle.transform.position, muzzle.transform.rotation);
         clone1.AddForce(muzzle.transform.forward * bulletSpeed, ForceMode.Impulse);
+        StartCoroutine(clone1.GetComponent<DamageDone>().BreakDistance());
+        currentAmmo--;
         yield return new WaitForSeconds(0.1f);
         Rigidbody clone2 = Instantiate(bullets, muzzle.transform.position, muzzle.transform.rotation);
         clone2.AddForce(muzzle.transform.forward * bulletSpeed, ForceMode.Impulse);
+        StartCoroutine(clone2.GetComponent<DamageDone>().BreakDistance());
+        currentAmmo--;
         yield return new WaitForSeconds(0.1f);
         Rigidbody clone3 = Instantiate(bullets, muzzle.transform.position, muzzle.transform.rotation);
         clone3.AddForce(muzzle.transform.forward * bulletSpeed, ForceMode.Impulse);
+        StartCoroutine(clone3.GetComponent<DamageDone>().BreakDistance());
+        currentAmmo--;
+        if (currentAmmo <= 0) base.noAmmo = true;
     }
     void Update()
     {
-
-        base.attackRange = range;
-        Debug.DrawRay(base.player.transform.position, base.player.transform.forward * attackRange, Color.red);
         if (Input.GetButtonDown("Fire2"))
         {
-            base.AimDownSight(false);
+            base.AimDownSight();
         }
-        if (Input.GetButtonDown("Fire1") && attackOnce)
+        if (Input.GetButtonDown("Fire1") && attackOnce && currentAmmo > 0)
         {
             StartCoroutine(ShootRifle());
             base.Attacking("Shoot", resetTimeShot);
-            RaycastHit hit;
-            if (Physics.Raycast(base.player.transform.position, base.player.transform.forward, out hit, attackRange))
-            {
-                if (hit.collider.tag == "Enemy")
-                {
-                    print("Done");
-
-                }
-            }
+            print(currentAmmo);
+        }
+        if(Input.GetButtonDown("Fire3"))
+        {
+            base.Reloading("ArIsAim");
+            currentAmmo = maxBullet;
         }
     }
 }
--- Assets/WeaponControl/PistolBehavior.cs
+++ Assets/WeaponControl/PistolBehavior.cs
@@ -9,37 +9,40 @@ public class PistolBehavior : Attack
     protected float bulletSpeed = 20;
     [SerializeField] private Rigidbody bullets;
     [SerializeField] private GameObject muzzle;
+    private int currentAmmo = 10;
 
-
+    private void Awake()
+    {
+        base.maxBullet = 10;
+        base.noAmmo = false;
+    }
 
     public void ShootPistol()
     {
         Rigidbody clone1 = Instantiate(bullets, muzzle.transform.position, muzzle.transform.rotation);
         clone1.AddForce(muzzle.transform.forward * bulletSpeed, ForceMode.Impulse);
+        StartCoroutine(clone1.GetComponent<DamageDone>().BreakDistance());
+        currentAmmo--;
+        if (currentAmmo <= 0) base.noAmmo = true;
     }
 
 
     void Update()
     {
-        base.attackRange = range;
-        Debug.DrawRay(base.player.transform.position, base.player.transform.forward * attackRange, Color.red);
         if(Input.GetButtonDown("Fire2"))
         {
-            base.AimDownSight(false);
+            base.AimDownSight();
         }
-        if (Input.GetButtonDown("Fire1") && attackOnce)
+        if (Input.GetButtonDown("Fire1") && attackOnce && currentAmmo > 0)
         {
-            
             base.Attacking("Shoot", resetTimeShot);
             ShootPistol();
-            RaycastHit hit;
-            if (Physics.Raycast(base.player.transform.position, base.player.transform.forward, out hit, attackRange))
-            {
-                if (hit.collider.tag == "Enemy")
-                {
-                    print("Done");
-                }
-            }
+            print(currentAmmo);
+        }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            base.Reloading("PistolIsAim");
+            currentAmmo = maxBullet;
         }
     }
 }
