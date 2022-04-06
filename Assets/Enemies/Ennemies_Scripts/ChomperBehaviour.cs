45978021552cb08b4f3c81b3402580706b658052
Pepen <ti-max__1998@hotmail.com>
2022-04-06 14:32:36 -0400

    update sound

--- Assets/Enemies/Ennemies_Scripts/ChomperBehaviour.cs
+++ Assets/Enemies/Ennemies_Scripts/ChomperBehaviour.cs
@@ -74,7 +74,6 @@ public class ChomperBehaviour : Enemie
     public void AttackBegin()
     {
         this.meleeAttackColl.enabled = true;
-        base.SetSound(this.meleeAttackSound);
     }
     public void AttackEnd()
     {
