44bdd305f93eb8e271a5de4deedbd244c1c04d30
Pepen <ti-max__1998@hotmail.com>
2022-04-12 14:33:07 -0400

    resolve txtmpPro conflit and possibilty to return to hub when in tutoriel level

--- Assets/Enemies/Ennemies_Scripts/Steven Scripts/Progress Saving/CheckPointManager.cs
+++ Assets/Enemies/Ennemies_Scripts/Steven Scripts/Progress Saving/CheckPointManager.cs
@@ -23,7 +23,6 @@ public class CheckPointManager : MonoBehaviour
             if (currentProgress.UpdateProgress(transform.position))
             {
                 this.playerStats.LastCheckpoint = transform.position;
-                playerStats.PlayerLevel = SceneManager.GetActiveScene().buildIndex;
             }
             else
             {
